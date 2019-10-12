83c9710d0b7346b9082fae2adb19bcb39f861f7f
ionred <bryanchaffin@bryanchaffin.com>
2019-10-11 20:49:02 -0500

    temporary patch for edge version weirdness

--- automatic/microsoft-edge-insider/update.ps1
+++ automatic/microsoft-edge-insider/update.ps1
@@ -20,6 +20,11 @@ function global:au_GetLatest {
     #tidy-5.1.25-win64.zip
     #$re  = "tidy-.+-win(32|64).zip"
     $version = ($version_page.Content -replace '\[\"', '[{"ver":"' -replace ',"', '}, {"ver":"' -replace ']', '}]' | ConvertFrom-Json) |Sort-Object -desc -Property { [version]$_.ver } | Select-Object -first 1 -ExpandProperty ver
+    if ($version.Substring(0,8) -eq '78.0.276') {
+        $a = [version]$version
+        $b = [string]$a.Revision
+        $version = "$($a.Major).$($a.Minor).$($a.Build).$($b.PadRight(10,'0'))"
+    }
     $download_page = Invoke-WebRequest -Uri $downloads 
     $url32 = ($download_page.AllElements | Where-Object data-download-enterpriseid -match "Beta32MSI" |Select-Object -first 1 -ExpandProperty data-download-url)
     $url64 = ($download_page.AllElements | Where-Object data-download-enterpriseid -match "Beta64MSI" |Select-Object -first 1 -ExpandProperty data-download-url)
