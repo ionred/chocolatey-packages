5cef596fa4844beb8f74c8447b5b487caa7e8694
ionred <ionred.dev@gmail.com>
2019-11-06 00:07:48 -0600

    remove hacky edge update patch

--- automatic/microsoft-edge-insider/update.ps1
+++ automatic/microsoft-edge-insider/update.ps1
@@ -20,16 +20,7 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   
-    $version_real=(Invoke-WebRequest "$($versionpage1)$($module_name)$($versionpage2)" -Method POST -ContentType 'application/json' -Body $params | ConvertFrom-Json).ContentID.Version
-
-    if ($version_real.Substring(0,8) -eq '78.0.276') {
-        $a = [version]$version_real
-        $b = [string]$a.Revision
-        $c = [string]$b.Padleft(8,'0')
-        $version = "$($a.Major).$($a.Minor).$($a.Build).20$($c)"
-    } else {
-        $version = $version_real
-    }
+    $version=(Invoke-WebRequest "$($versionpage1)$($module_name)$($versionpage2)" -Method POST -ContentType 'application/json' -Body $params | ConvertFrom-Json).ContentID.Version
     $download_page = Invoke-WebRequest -Uri $downloads 
     $url_32 = ($download_page.AllElements | Where-Object data-download-enterpriseid -match $download_module_name32 |Select-Object -first 1 -ExpandProperty data-download-url)
     $url_64 = ($download_page.AllElements | Where-Object data-download-enterpriseid -match $download_module_name64 |Select-Object -first 1 -ExpandProperty data-download-url)
