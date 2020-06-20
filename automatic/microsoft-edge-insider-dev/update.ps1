ac6c7e10da220998a61f1e7f96988eb530de7193
ionred <ionred@users.noreply.github.com>
2020-06-19 23:22:12 -0500

    fix updater

--- automatic/microsoft-edge-insider-dev/update.ps1
+++ automatic/microsoft-edge-insider-dev/update.ps1
@@ -1,11 +1,11 @@
 import-module au
-$module_name = 'msedge-dev-win-x64'
-$download_module_name32 = 'Dev32MSI'
-$download_module_name64 = 'Dev64MSI'
-$versionpage1 = 'https://msedge.api.cdp.microsoft.com/api/v1/contents/Browser/namespaces/Default/names/'
-$versionpage2 = '/versions/latest?action=select'
-$downloads = 'https://www.microsoftedgeinsider.com/en-us/enterprise'
-$params = '{"targetingAttributes": {}}'
+#$module_name = 'msedge-beta-win-x64'
+#$download_module_name32 = 'Beta32MSI'
+#$download_module_name64 = 'Beta64MSI'
+$versionpage1 = 'https://edgeupdates.microsoft.com/api/products'
+#$versionpage2 = '/versions/latest?action=select'
+#$downloads = 'https://www.microsoftedgeinsider.com/en-us/enterprise'
+#$params = '{"targetingAttributes": {}}'
 
 function global:au_SearchReplace {
     @{
@@ -20,21 +20,15 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
   
-    $version_real=(Invoke-WebRequest "$($versionpage1)$($module_name)$($versionpage2)" -Method POST -ContentType 'application/json' -Body $params | ConvertFrom-Json).ContentID.Version
-
-    if ($version_real.Substring(0,8) -eq '78.0.276') {
-        $a = [version]$version_real
-        $b = [string]$a.Revision
-        $c = [int32]$b.PadRight(10,'0')
-        $version = "$($a.Major).$($a.Minor).$($a.Build).$($c)"
-    } else {
-        $version = $version_real
-    }
-    $download_page = Invoke-WebRequest -Uri $downloads 
-    $url_32 = ($download_page.AllElements | Where-Object data-download-enterpriseid -match $download_module_name32 |Select-Object -first 1 -ExpandProperty data-download-url)
-    $url_64 = ($download_page.AllElements | Where-Object data-download-enterpriseid -match $download_module_name64 |Select-Object -first 1 -ExpandProperty data-download-url)
-    $url32 = (Invoke-WebRequest -Uri $url_32 -MaximumRedirection 0 -ErrorAction 'Silent').Headers.Location
-    $url64 = (Invoke-WebRequest -Uri $url_64 -MaximumRedirection 0 -ErrorAction 'Silent').Headers.Location
+    $version_page_data = (Invoke-WebRequest -Uri $versionpage1).Content | ConvertFrom-Json
+    #$version=(Invoke-WebRequest "$($versionpage1)$($module_name)$($versionpage2)" -Method POST -ContentType 'application/json' -Body $params | ConvertFrom-Json).ContentID.Version
+    $releases = $version_page_data | Where-Object -Property Product -eq Dev | Select-Object Releases
+    $version = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x64'}).ProductVersion
+    #$download_page = Invoke-WebRequest -Uri $downloads 
+    #$url_32 = ($download_page.AllElements | Where-Object data-download-enterpriseid -match $download_module_name32 |Select-Object -first 1 -ExpandProperty data-download-url)
+    #$url_64 = ($download_page.AllElements | Where-Object data-download-enterpriseid -match $download_module_name64 |Select-Object -first 1 -ExpandProperty data-download-url)
+    $url32 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x86'}).Artifacts.Location
+    $url64 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x64'}).Artifacts.Location
     
     $Latest = @{URL32 = $url32; URL64 = $url64; Version = $version; }
     return $Latest
