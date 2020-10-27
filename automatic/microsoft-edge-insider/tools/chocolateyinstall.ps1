15a94cd8e017546306f7d84be4827104edef9b39
ionred <ionred.dev@gmail.com>
2020-10-27 00:02:09 +0000

    AU: 1 updated - microsoft-edge-insider [skip ci] https://gist.github.com/fd371814a0847427a5a07a141abeec9a/00109a10d3a2ff806bb6c6bb2f22721e736ccf61

--- automatic/microsoft-edge-insider/tools/chocolateyinstall.ps1
+++ automatic/microsoft-edge-insider/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
-$url32        = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/ca513e60-714d-403b-ab61-02fbcb84acdb/MicrosoftEdgeBetaEnterpriseX86.msi'
-$url64      = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/12344135-7b7d-43e4-8eab-0de2110c0ed2/MicrosoftEdgeBetaEnterpriseX64.msi'
+$url32        = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/5d53a352-c4c8-48c1-a836-ca57fc6e095d/MicrosoftEdgeBetaEnterpriseX86.msi'
+$url64      = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/015cb769-a7d6-428f-8717-4b10a7451b57/MicrosoftEdgeBetaEnterpriseX64.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,9 +11,9 @@ $packageArgs = @{
 
   softwareName  = 'Microsoft Edge Beta'
 
-  checksum32    = '84f98ec7c01a7d3aeacc11c2cc81887f14859b43cde8a43c8bda58a19a8a6656'
+  checksum32    = '96409924236d61bb7de10e9b909131a70ce27bd1e719f254e619abb0f7336517'
   checksumType  = 'sha256'
-  checksum64    = 'e467e0b53ed4c0d60cae182d4055d6266491e9173de6d9cae3a49d789f542b9c'
+  checksum64    = 'fd4365996575190a8f8bd5daa5db135fe940e8816e77009e9ebd7a8c40e9b366'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
