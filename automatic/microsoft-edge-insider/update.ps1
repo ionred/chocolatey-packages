031d824063d7c676cc07de90acb041f98b5aabe1
ionred <ionred@users.noreply.github.com>
2024-02-12 23:17:02 -0600

    AU: 1 updated - microsoft-edge-insider [skip ci] https://gist.github.com/ionred/fd371814a0847427a5a07a141abeec9a/1dbea3685747c105ab1b4fc1f68fc8a822349f0c

--- automatic/microsoft-edge-insider/update.ps1
+++ automatic/microsoft-edge-insider/update.ps1
@@ -3,26 +3,33 @@ $versionpage1 = 'https://edgeupdates.microsoft.com/api/products'
 $branch = 'Beta'
 
 function global:au_SearchReplace {
-    @{
-        'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
-            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
-        }
-     }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url32bit\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^[$]version\s*=\s*)('.*')" = "`$1'$($Latest.RemoteVersion)'"
+    }
+  }
 }
 
 function global:au_GetLatest {
     $version_page_data = (Invoke-WebRequest -Uri $versionpage1).Content | ConvertFrom-Json
     $releases = $version_page_data | Where-Object -Property Product -eq $branch | Select-Object Releases
     $version = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x64'}).ProductVersion
-    $url32 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x86'}).Artifacts.Location
-    $url64 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x64'}).Artifacts.Location  
+    $URL32 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x86'}).Artifacts.Location
+    $URL64 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x64'}).Artifacts.Location  
     $Checksum64 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x64'}).Artifacts.Hash
     $Checksum32 = ($releases.Releases | Where-Object {$_.Platform -eq 'Windows'} | Where-Object {$_.Architecture -eq 'x86'}).Artifacts.Hash
-    $Latest = @{URL32 = $url32; URL64 = $url64; Version = $version; Checksum32 = $Checksum32; Checksum64 = $Checksum64 }
-    return $Latest
+    
+    @{
+      URL32 = $url32; 
+      URL64 = $url64; 
+      Version = $version; 
+      RemoteVersion = $version;
+     }
 }
+
 write-host 'end'
-update
+update -ChecksumFor none
