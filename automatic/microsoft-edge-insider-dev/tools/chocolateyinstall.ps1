d4112363c44e599876819e08c5984da2f5054226
ionred <ionred.dev@gmail.com>
2024-05-01 00:02:29 +0000

    AU: 1 updated - microsoft-edge-insider-dev [skip ci] https://gist.github.com/ionred/fd371814a0847427a5a07a141abeec9a/b6eb89672d49a4e0d6cfe777b546862fb241c961

--- automatic/microsoft-edge-insider-dev/tools/chocolateyinstall.ps1
+++ automatic/microsoft-edge-insider-dev/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$version = '125.0.2535.6'
+$version = '126.0.2552.0'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
-  url32bit      = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/7ee29800-082f-41de-bf78-67aba5b9cd14/MicrosoftEdgeDevEnterpriseX86.msi'
-  url64bit      = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/1b8d1249-40db-4486-b387-c5d40373444b/MicrosoftEdgeDevEnterpriseX64.msi'
+  url32bit      = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/46501de1-d83f-4484-93ad-0c4c7625607b/MicrosoftEdgeDevEnterpriseX86.msi'
+  url64bit      = 'https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/0185f7de-763e-4379-a2ca-b02f9d3f121a/MicrosoftEdgeDevEnterpriseX64.msi'
   softwareName  = 'Microsoft Edge Dev'
-  checksum32    = 'D90DD2C284B7A7B7CF37ADD78A96C246E121E4D245873769F82B80806B6E5CA1'
+  checksum32    = 'B873273C5282C31DFFF96D7F584CBD362CF4A4D83EEA1EA8A46505F64B4274FA'
   checksumType  = 'sha256'
-  checksum64    = '9925FF726FE9E4B2EFE50536855D7A8DA1406ECE027AC6D24AD4F3E204E612DC'
+  checksum64    = '77DEBF4CDEBF979CD7A99C89FA937E759BB150707A8BF8565FDD6416C61F1AEA'
   checksumType64= 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
